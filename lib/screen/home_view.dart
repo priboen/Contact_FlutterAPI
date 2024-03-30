d8ccdb5f1ae83d3e2531f45d74160b8f83a1dc8d
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-03-30 21:56:45 +0700

    create floating action button for create a data

--- lib/screen/home_view.dart
+++ lib/screen/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:data_kontak/controller/kontak_controller.dart';
 import 'package:data_kontak/model/kontak.dart';
+import 'package:data_kontak/widget/form_kontak.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
@@ -20,10 +21,10 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Daftar Orang"),
-      ),
-      body: FutureBuilder<List<Kontak>>(
+        appBar: AppBar(
+          title: const Text("Daftar Orang"),
+        ),
+        body: FutureBuilder<List<Kontak>>(
           future: _controller.getPeople(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
@@ -36,19 +37,27 @@ class _HomeViewState extends State<HomeView> {
               );
             } else {
               return ListView.builder(
-                  itemCount: snapshot.data?.length ?? 0,
-                  itemBuilder: (context, index) {
-                    Kontak person = snapshot.data![index];
-                    return ListTile(
-                      title: Text(person.nama),
-                      subtitle: Text(person.email),
-                      leading: CircleAvatar(
-                        backgroundImage: NetworkImage(person.foto),
-                      ),
-                    );
-                  });
+                itemCount: snapshot.data?.length ?? 0,
+                itemBuilder: (context, index) {
+                  Kontak person = snapshot.data![index];
+                  return ListTile(
+                    title: Text(person.nama),
+                    subtitle: Text(person.email),
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(person.foto),
+                    ),
+                  );
+                },
+              );
             }
-          }),
-    );
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const FormKontak()));
+          },
+          child: const Icon(Icons.add),
+        ));
   }
 }
