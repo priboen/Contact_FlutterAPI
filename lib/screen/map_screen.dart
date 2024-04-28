ba1b701d314f5b82c3562590cd5865f3bba1ef08
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-04-28 19:54:12 +0700

    declare final function

---
+++ lib/screen/map_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapScreen extends StatefulWidget {
+  final Function(String) onLocationSelected;
+  const MapScreen({super.key, required this.onLocationSelected});
+
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  late GoogleMapController mapController;
+  LatLng? _lastMapPosition;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
